ffaaa01272ad2e0155bd389fc587cf340507e213
nicmyers <nicmyers@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-21 02:28:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/where-ive-been/trunk@103881 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -6,51 +6,46 @@ Requires at least: 2.7
 Tested up to: 2.7.1
 Stable tag: trunk
 
-Where I've Been embeds your whereivebeen.com maps on your Wordpress blog. 
+Where I've Been embeds your whereivebeen.com maps on your Wordpress blog.
 
 == Description ==
 
-Where I've Been embeds your whereivebeen.com maps on your Wordpress blog. 
+Where I've Been embeds your whereivebeen.com maps on your Wordpress blog.
 
-I decided to create this plugin after searching for an easy way to embed Where I've Been maps on Wordpress blogs and not finding one.
+I created this plugin after searching for an easy way to embed whereivebeen.com maps on Wordpress blogs and not finding one.
+Please visit the plugin homepage (http://www.nicmyers.com/where-ive-been-wordpress-plugin/) for updates and more information.
 
-Please visit http://www.nicmyers.com/where-ive-been-wordpress-plugin/ for updates and more information
-
-This plugin is not affiliated with whereivebeen.com.
+*0.6.0 New Feature*
+You can now change the width of your map by adding the width="" attribute to your shortcode. The height is automaticaly calculated for you.
 
 
 == Installation ==
 
 1. Download and extract the "where-ive-been" folder.
 2. Upload the "where-ive-been" folder to your WordPress plugin directory, usually "wp-content/plugins".
-3. Activate the plugin in your WordPress admin panel
+3. Activate the plugin in your WordPress admin panel.
 4. *Optional* If you wish, you may hard code your user id's into the where-ive-been.php file. I only recommend doing this if you know what you're doing.
-5. Place the [whereivebeen uid="" iid="" ] with the required values filled in wherever you wish for your map to be displayed. See usage for more info on how to configure the plugin.
+5. Place the shortcode with the required values filled in wherever you wish for your map to be displayed. See usage for more info on how to configure the plugin.
 
-Update the plugin simply use Wordpress's built in one-click updater. If you have to upgrade manually simply repeat the installation steps.
 
 == Usage ==
 
 Before you can embed the map on your blog, you will need to retrieve two user id's found in the source code of your whereivebeen.com profile.
 
-1. Go to http://www.whereivebeen.com and log in to your account. 
+1. Go to http://www.whereivebeen.com and log in to your account.
 2. Click on the "Profile" or "My Profile" buttons to pull up your map.
 3. Right click in your browser and select "View Page Source"(Firefox), "View Source" (Internet Explorer & Safari).
 4. Search the code (CTRL-f Windows, Command-f Mac) first for "uID" and then for "iID" and record each of the corresponding values that are single quotation marks.
-	Example:  Person = { **uID: '0000000'**, sID: 'Not Needed', **iID: '00000000000000000000000000000000'** )  
-	You are looking for the values that are represented as 0's in the example. The uID will be around 8 digits long and the iID around 32 digits.
+    Example:  Person = { **uID: '0000000'**, sID: 'Not Needed', **iID: '00000000000000000000000000000000'** )  
+    You are looking for the values that are represented as 0's in the example. The uID will be around 8 digits long and the iID around 32 digits.
 
+The plugin makes use of Wordpress Shortcodes and has two required options and two optional ones.
+Embed maps into your post or page using shortcode: [whereivebeen uid="Required" iid="Required" roundcorners="Optional" width="Optional"]
 
-The plugin makes use of Wordpress Shortcodes and has 2 required options and one optional one.
-
-[whereivebeen uid="uID number REQUIRED" iid="iID number REQUIRED" roundcorners="true or false OPTIONAL"]
-
-1. uid = *Required* Your unique 8 digit uID found on your whereivebeen.com profile (see above).
-2. iid = *Required* Your unique 8 digit iID found on your whereivebeen.com profile (see above).
-3. roundcorners = Optional. This option can be set to either 'true' or 'false', the default is 'true'. This option will change the corners on the map window to either round(true) or square(false).
-
-To use the shortcode on a post or page place the [whereivebeen uid="" iid="" ] with the required values filled in wherever you wish for your map to be displayed. 
-
+1. uid = *Required* Your unique 8 digit uID found on your whereivebeen.com profile (see readme).
+2. iid = *Required* Your unique 8 digit iID found on your whereivebeen.com profile (see readme).
+3. roundcorners = *Optional* This option can be set to either 'true' or 'false', the default is 'true'. This option will change the corners on the map window to either round(true) or square(false).
+4. width = *Optional* Set the width if the default (570x360) is too wide for your blog. The height is automatically calculated.
 
 
 == Frequently Asked Questions ==
@@ -67,10 +62,16 @@ I have personally only tested the plugin on Wordpress 2.7+ installations. The pl
 ==Updates ==
 
 Updates will be posted here and at http://www.nicmyers.com/where-ive-been-wordpress-plugin/
-
+To update the plugin simply use Wordpress's built in one-click updater. If you wish to upgrade manually simply repeat the installation steps.
 
 == History ==
+0.6.0 [2009.03.18]
+
+* Added: Option to change the width of the map
+* Minor code fixes and readme readability updates
+
 0.5.1 [2009.03.18]
+
 * Minor small fixes
 
 0.5 [2009.03.18]
@@ -83,6 +84,6 @@ Updates will be posted here and at http://www.nicmyers.com/where-ive-been-wordpr
 
 * Added: Shortcode support for uID and iID
 
-0.1-0.3
+0.1-0.3 [2009.03]
 
 * Initial development phase
--- where-ive-been.php
+++ where-ive-been.php
@@ -2,24 +2,21 @@
 /*
 Plugin Name: Where I've Been
 Plugin URI: http://www.nicmyers.com/where-ive-been-wordpress-plugin/
-Description: Plugin for displaying WhereIveBeen.com maps in your Wordpress blog. Use: [whereivebeen uid='' iid=''] to embed into your post or page. Both values are required. To get them, view the source of your whereivebeen.com profile page and search for uID and iID. See the readme for more info.
+Description: Plugin for displaying WhereIveBeen.com maps in your Wordpress blog. Use: [whereivebeen uid='' iid=''] to embed into your post or page. Both values are required. To get them, view the source of your whereivebeen.com profile page and search for uID and iID. See the readme for more options.
 Author: Nic Myers
-Version: 0.5.1
+Version: 0.6.0
 Author URI: http://www.nicmyers.com
 */
-
 /*  Copyright 2009  Nic Myers  (email : nicmyers@w3designs.net) 
 
 Where I've Been embeds your whereivebeen.com maps on your Wordpress blog. 
-
-I decided to create this plugin after searching for an easy way to embed Where I've Been maps on Wordpress blogs and not finding one.
-
-Embed into your post using [whereivebeen uid='' iid=''] and setting the required settings.
+Embed into your post or page using shortcode: [whereivebeen uid="Required" iid="Required" roundcorners="Optional" width="Optional"]
 
 == Shortcode Options ==
 1. uid = *Required* Your unique 8 digit uID found on your whereivebeen.com profile (see readme).
 2. iid = *Required* Your unique 8 digit iID found on your whereivebeen.com profile (see readme).
 3. roundcorners = *Optional* This option can be set to either 'true' or 'false', the default is 'true'. This option will change the corners on the map window to either round(true) or square(false).
+4. width = *Optional* Set the width if the default (570x360) is too wide for your blog. The height is automatically calculated. 
 
 */
 
@@ -28,8 +25,7 @@ $wib_uID = '';
 $wib_iID = '';
 $wib_roundcorners = 'true'; 	//True or False
 
-//This is the code that outputs the map. 
-//The shortcode is [whereivebeen uid="uID number" iid="iID number" roundcorners="true or false"]
+//This is the code that outputs the map.
 function whereivebeen_output($atts, $content=null) {
 
 	global $wib_uID;
@@ -40,20 +36,18 @@ function whereivebeen_output($atts, $content=null) {
       'uid' => '',
       'iid' => '',
 	  'roundcorners' => 'true',
+	  'width' => '570',
       ), $atts ) );
 	  
 	$wib_uID = $uid;
 	$wib_iID = $iid;
 	$wib_roundcorners = $roundcorners;
-	
-	//
-	//echo $wib_uID;
-	//echo $wib_iID;
+	$height	= $width / 1.583;
 	
 	ob_start();
 	print <<< SWF
-<div class="whereivebeen" style="width:570px;">
-<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" id="viewMap"align="middle" width="570" height="360"><param name="movie" value="http://static1.whereivebeen.com/swf/3.4/view.swf"><param name="salign" value="ct"><param name="quality" value="high"><param name="wmode" value="transparent"><param name="flashvars" value="env=production&amp;network=wib&amp;networkURL=http://www.whereivebeen.com/&amp;uID={$wib_uID}&amp;iID={$wib_iID}&amp;showroundedcorners={$wib_roundcorners}"><param name="allowScriptAccess" value="always"><param name="scale" value="noscale"><param name="bgcolor" value="#ffffff"><embed src="http://static1.whereivebeen.com/swf/3.4/view.swf" wmode="transparent" flashvars="env=production&amp;network=wib&amp;networkURL=http://www.whereivebeen.com/&amp;uID={$wib_uID}&amp;iID={$wib_iID}&amp;showroundedcorners={$wib_roundcorners}" quality="high" scale="noscale" salign="ct" bgcolor="#ffffff" name="wib-view-map" allowscriptaccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" align="middle" width="570" height="360"></embed></object>
+<div class="whereivebeen" style="width:{$width}px;">
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" id="viewMap"align="middle" width="{$width}" height="{$height}"><param name="movie" value="http://static1.whereivebeen.com/swf/3.4/view.swf"><param name="salign" value="ct"><param name="quality" value="high"><param name="wmode" value="transparent"><param name="flashvars" value="env=production&amp;network=wib&amp;networkURL=http://www.whereivebeen.com/&amp;uID={$wib_uID}&amp;iID={$wib_iID}&amp;showroundedcorners={$wib_roundcorners}"><param name="allowScriptAccess" value="always"><param name="scale" value="noscale"><param name="bgcolor" value="#ffffff"><embed src="http://static1.whereivebeen.com/swf/3.4/view.swf" wmode="transparent" flashvars="env=production&amp;network=wib&amp;networkURL=http://www.whereivebeen.com/&amp;uID={$wib_uID}&amp;iID={$wib_iID}&amp;showroundedcorners={$wib_roundcorners}" quality="high" scale="noscale" salign="ct" bgcolor="#ffffff" name="wib-view-map" allowscriptaccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" align="middle" width="{$width}" height="{$height}"></embed></object>
 <a href="http://www.whereivebeen.com" class="wib_mainsite" style="color: #d8dfea; text-decoration: none; font: .75em sans-serif; float: left;">WhereIveBeen.com</a><a href="http://www.nicmyers.com/where-ive-been-wordpress-plugin/" alt="Travel Photographer" class="wib_pluginauthor" style="color: #d8dfea; font: .75em sans-serif; text-decoration: none; float: right;">Get the Where I've Been Wordpress Plugin</a>
 <p style="clear: both;">&nbsp;</p>
 </div>
@@ -62,9 +56,7 @@ SWF;
 	ob_end_clean();
 
 	return $wib_output;
-
 }
 
 add_shortcode('whereivebeen', 'whereivebeen_output');
-
 ?>
